d9464ccc876d096152a889c0cd1b33908f142df0
franclobo <fjbl2788@gmail.com>
2022-06-23 12:24:10 -0500

    Write book's actions and reducer (in /src/redux/books/books.js) using the ducks pattern.

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,28 +1,36 @@
-const prevData = {
-  books: []
-}
+// Types
+const ADDING_BOOKS_SUCCESFUL = 'ADDING_BOOKS_SUCCESFUL';
+const REMOVING_BOOKS_SUCCESFUL = 'REMOVING_BOOKS_SUCCESFUL';
 
-const OBTAIN_BOOKS_SUCCESFUL = 'OBTAIN_BOOKS_SUCCESFUL'
-
-// Reducers
-export default function booksReducer(state = prevData, action) {
-  switch (action.type) {
-    case OBTAIN_BOOKS_SUCCESFUL:
-      return { ...state, books: action.payload }
-    default:
-      return state
-  }
-}
+// Initial state
+const initialState = {
+  books: [],
+};
 
 // Actions
-export const obtainBooks = () => async (dispatch, getState) => {
-  try {
-    const res = await axios.get('')
-    dispatch({
-      type: OBTAIN_BOOKS_SUCCESFUL,
-      payload: res.data.results
-    })
-  } catch (error) {
-    console.log(error)
+export const addBooks = () => (dispatch, getState) => {
+  getState();
+  dispatch({
+    type: ADDING_BOOKS_SUCCESFUL,
+    payload: this.books.push(),
+  });
+};
+export const removeBooks = (index) => (dispatch, getState) => {
+  getState();
+  dispatch({
+    type: ADDING_BOOKS_SUCCESFUL,
+    payload: this.books.splice(index, 1),
+  });
+};
+
+// Reducers
+export default function booksReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADDING_BOOKS_SUCCESFUL:
+      return { ...state, books: action.payload };
+    case REMOVING_BOOKS_SUCCESFUL:
+      return { ...state, books: action.payload };
+    default:
+      return state;
   }
 }
