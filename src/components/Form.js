e84ea20c42c3267b9e31a945eb516d2e165a26fa
franclobo <fjbl2788@gmail.com>
2022-06-24 17:58:29 -0500

    Edit Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,42 +1,38 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 import { addBooks } from '../redux/books/Books';
 
 function InputBook() {
   const dispatch = useDispatch();
-  const [book, setBook] = useState({
-    id: uuidv4(),
-    title: '',
-    author: '',
-  });
-
-  const onChange = (e) => {
-    const [name, value] = [e.target.name, e.target.value];
-    if (value.trim() === '')e.target.value = '';
-    setBook((previous) => ({
-      ...previous,
-      [name]: value.trim(),
-    }));
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeAuthor = (e) => {
+    setTitle(e.target.value);
+    setAuthor(e.target.value);
   };
 
   return (
-    <form onSubmit={() => dispatch(addBooks({ ...book }))} className="form-book">
+    <form onSubmit={() => dispatch(addBooks())} className="form-book">
       <input
         type="text"
         className="input-title"
         placeholder="Add title..."
         required
+        value={title}
         name="title"
-        onChange={onChange}
+        onChange={onChangeTitle}
       />
       <input
         type="text"
         className="input-author"
         placeholder="Add author..."
         required
+        value={author}
         name="author"
-        onChange={onChange}
+        onChange={onChangeAuthor}
       />
       <button type="submit">Add Book</button>
     </form>
